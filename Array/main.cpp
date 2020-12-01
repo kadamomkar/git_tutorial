56c58d8c732d4065aa781096056e728eaa7e12ca
kadamomkar <kadam.omkar@outlook.com>
2020-12-01 13:58:10 +0530

    1st commit

--- Array/main.cpp
+++ Array/main.cpp
@@ -6,9 +6,65 @@
 //
 
 #include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace::std;
+
+struct ar
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+void display(struct ar arr)
+{
+    for (int i=0;i<arr.length;i++)
+    cout<<arr.A[i];
+}
+
+void append(struct ar *arr,int x)
+{
+    if(arr->length<arr->size)
+    {arr->A[arr->length]=x;
+        arr->length++;
+    }
+}
+
+void insert(struct ar *arr,int p,int x)
+{
+    if(p>=0&&p<=arr->length)
+    {
+        for(int i=arr->length;i>=p;i--)
+        {
+            arr->A[i]=arr->A[i-1];
+        }
+        arr->A[p-1]=x;
+        arr->length++;
+    }
+}
+
+void del(struct ar *arr,int p)
+{
+    if(p>=0&&p<arr->length)
+    {int i;
+        for(i=p;i<arr->length;i++)
+        {
+            arr->A[i-1]=arr->A[i];
+        }
+        arr->A[i-1]=0;
+        arr->length--;
+    }
+}
+
+
+int main()
+{
+    struct ar arr={{0,1,2,3,4},10,5};
+//    append(&arr,10);
+//    insert(&arr,3,10);
+    del(&arr,3);
+    display(arr);
     return 0;
 }
